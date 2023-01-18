b71fda2cd1274b398f658f8dc35b5a8d35cc82c6
~Gonzalo <gonzalo00396@gmail.com>
2023-01-18 11:50:42 -0300

    Elimino

--- Ticmas - Curso Desarrollo FrontEnd/my_script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('boton_contacto').addEventListener('click', function() {
-	
-	if(document.getElementById('box_model_contacto').style.visibility == "hidden"){
-		document.getElementById('box_model_contacto').style.visibility = "visible";
-	}else{
-		document.getElementById('box_model_contacto').style.visibility = "hidden";
-	}
-	
-})
